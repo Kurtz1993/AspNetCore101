826acccd74182f04ed28055f6dc3e12321040dab
Luis Rogelio Hernández López <luis.hlopez@outlook.com>
2019-09-27 18:09:41 -0500

    Add styles and implement index.cshtml

--- ContosoCraft.Website/Pages/Index.cshtml.cs
+++ ContosoCraft.Website/Pages/Index.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ContosoCraft.Website.Models;
+using ContosoCraft.WebSite.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
@@ -10,16 +12,23 @@ namespace ContosoCraft.Website.Pages
 {
     public class IndexModel : PageModel
     {
+        public IEnumerable<Product> Products { get; private set; }
+
+        private readonly JsonFileProductsService _productsService;
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(
+            ILogger<IndexModel> logger,
+            JsonFileProductsService productsService
+        )
         {
+            _productsService = productsService;
             _logger = logger;
         }
 
         public void OnGet()
         {
-
+            Products = _productsService.GetProducts();
         }
     }
 }
